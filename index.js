7a9b3806797e0de77b802d0b39ee09f5dda25112
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-17 14:44:39 -0400

    Store the flick in an object

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const app = {
     init: function(formSelector){
+        this.max=0
         document
            .querySelector(formSelector)
-           .addEventListener('submit',this.handleSubmit)
+           .addEventListener('submit',(ev)=>{
+                ev.preventDefault()
+                this.handleSubmit(ev)
+           })
     },
 
     handleSubmit: function(ev){
-        ev.preventDefault()
         const f = ev.target
-        console.log(f.flickName.value)
+        const flick = {
+            id: ++this.max,
+            name: f.flickName.value,
+        }
+        console.log(flick)
+        f.reset()
     },
 }
 app.init('#flickForm')

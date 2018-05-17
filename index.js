76c335aaf2bdb8beeba1bad1423d12ade29223ac
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-17 15:11:51 -0400

    Use ES6 method definition syntax

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const app = {
            //OR CAN DO THIS: .addEventListener('submit', this.handleSubmit.bind(this))
     },
 
-    renderListItem: function(flick){
+    renderListItem(flick){
         const item = document.createElement('li')
         item.textContent = flick.name
         return item
     },
 
-    handleSubmit: function(ev){
+    handleSubmit(ev){
         const f = ev.target
         const flick = {
             id: ++this.max,
